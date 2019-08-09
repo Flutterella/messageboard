786d2c37f84f571372ea60b4607534eaab3fbf8e
Bullens Lena <BullensLena@gmail.com>
2019-08-09 10:19:37 +0200

    fix bug when trying to display empty message board

--- src/main/java/be/intecbrussel/messageboard/controller/BoardController.java
+++ src/main/java/be/intecbrussel/messageboard/controller/BoardController.java
@@ -2,6 +2,7 @@ package be.intecbrussel.messageboard.controller;
 
 import be.intecbrussel.messageboard.model.Message;
 import be.intecbrussel.messageboard.service.MessageService;
+import be.intecbrussel.messageboard.service.NoMessageFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,14 +23,18 @@ public class BoardController {
 
     @GetMapping("/board")
     public String board(Model model) {
+        List<Message> messages;
         try {
-            List<Message> messages = messageService.getAllMessages();
+            messages = messageService.getAllMessages();
             Collections.reverse(messages);
             model.addAttribute("messages", messages);
             model.addAttribute("messageForm", new MessageForm());
             return "board";
-        } catch (Exception e) {
-            return "";
+        } catch (NoMessageFoundException e) {
+            messages = new ArrayList<>();
+            model.addAttribute("messages", messages);
+            model.addAttribute("messageForm", new MessageForm());
+            return "board";
         }
     }
 
